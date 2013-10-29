bbeeb8287b1159635e8c0352dd4d8003d8077c69
Alex Ford <Alex.Ford@CodeTunnel.com>
2013-10-29 12:29:36 -0600

    blah blah azure blah

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var http = require('http');
+var http = require('http'),
+    util = require('util');
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(util.inspect(process.env) + '\n');
 }).listen(process.env.PORT || 1337);
 console.log('Server running at http://127.0.0.1:1337/');
 
