dd3c80b633563cb627359b6a630e75c7e2814428
Alex Ford <Alex.Ford@CodeTunnel.com>
2013-10-29 12:14:00 -0600

    More azure stuff.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
-}).listen(process.env.PORT || 1337, '127.0.0.1');
+}).listen(process.env.PORT || 1337);
 console.log('Server running at http://127.0.0.1:1337/');
 
 ///*
