b214f3e996b40857732164f829bbff0503f8deb2
Alex Ford <alex.ford@codetunnel.com>
2013-11-04 01:25:31 -0700

    Minor updates.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 */
 
 // Dependencies
-var db = require('mongoose-dbwrapper'),
+var dbwrapper = require('mongoose-dbwrapper'),
     fs = require('fs'),
     http = require('http'),
     jade = require('jade'),
@@ -21,63 +21,61 @@ var db = require('mongoose-dbwrapper'),
 require('./utilities/prototypeHelpers')();
 
 // Initialize the database module.
-db.init({
-        connectionString: process.env.CONNECTION_STRING.toString()
-    },
+dbwrapper.init(
+    { connectionString: process.env.CONNECTION_STRING.toString() },
     function (db) {
+        // Initialize node-static, a static file serving module.
+        var file = new nodeStatic.Server('./public'),
+            port = process.env.PORT || 3000;
 
-    // Initialize node-static, a static file serving module.
-    var file = new nodeStatic.Server('./public'),
-        port = process.env.PORT || 3000;
+        // Create basic http server.
+        var server = http.createServer(function (request, response) {
+            // When the request is finished, serve files.
+            request.addListener('end', function () {
+                // If the request is for the root URL then compile the index Jade view and serve it...
+                if (request.url === '/') {
+                    var jadeFn = jade.compile(
+                        fs.readFileSync(path.resolve('views', 'index.jade')),
+                        { filename: path.resolve('views', 'index.jade') }
+                    );
+                    response.writeHead(200, { "Content-Type": "text/html" });
+                    response.end(jadeFn());
+                }
+                else if (request.url === '/styles.css') {
+                    var stylusFile = fs.readFileSync(path.resolve('public', 'styles.styl'));
+                    stylus(stylusFile.toString()).render(function(err, css) {
+                        if (err) return console.error(err);
+                        response.writeHead(200, { "Content-Type": "text/css" });
+                        response.end(css);
+                    });
+                }
+                // ...otherwise let node-static match the request to static files in the "public" directory.
+                else
+                    file.serve(request, response);
+            }).resume();
+        }).listen(port, function () {
+            console.log("Listening at http://localhost:%s", port);
+        });
 
-    // Create basic http server.
-    var server = http.createServer(function (request, response) {
-        // When the request is finished, serve files.
-        request.addListener('end', function () {
-            // If the request is for the root URL then compile the index Jade view and serve it...
-            if (request.url === '/') {
-                var jadeFn = jade.compile(
-                    fs.readFileSync(path.resolve('views', 'index.jade')),
-                    { filename: path.resolve('views', 'index.jade') }
-                );
-                response.writeHead(200, { "Content-Type": "text/html" });
-                response.end(jadeFn());
-            }
-            else if (request.url === '/styles.css') {
-                var stylusFile = fs.readFileSync(path.resolve('public', 'styles.styl'));
-                stylus(stylusFile.toString()).render(function(err, css) {
-                    if (err) return console.error(err);
-                    response.writeHead(200, { "Content-Type": "text/css" });
-                    response.end(css);
-                });
-            }
-            // ...otherwise let node-static match the request to static files in the "public" directory.
-            else
-                file.serve(request, response);
-        }).resume();
-    }).listen(port, function () {
-        console.log("Listening at http://localhost:%s", port);
-    });
+        // Configure shotgun and shotgun-client modules.
+        var shotgun = require('shotgun'),
+            shell = new shotgun.Shell({
+                debug: process.env.DEBUG,
+                // Rather than define access functions on each command module we will specify the default access for all
+                // command modules on the shell and set them to the shell helper function we created for checking command access.
+                defaultCmdAccess: function (shell, cmdName) {
+                    return shell.canAccessCmd(cmdName);
+                }
+            }),
+            shotgunClient = require('shotgun-client');
 
-    // Configure shotgun and shotgun-client modules.
-    var shotgun = require('shotgun'),
-        shell = new shotgun.Shell({
-            debug: process.env.DEBUG,
-            // Rather than define access functions on each command module we will specify the default access for all
-            // command modules on the shell and set them to the shell helper function we created for checking command access.
-            defaultCmdAccess: function (shell, cmdName) {
-                return shell.canAccessCmd(cmdName);
-            }
-        }),
-        shotgunClient = require('shotgun-client');
+        // Attach custom functions to the shell so they can be used in our command modules for U413.
+        extend(shell, shellFunctions);
+        shell.db = db;
 
-    // Attach custom functions to the shell so they can be used in our command modules for U413.
-    extend(shell, shellFunctions);
-    shell.db = db;
+        // Attach shotgun-client to the http server so it can listen for connections.
+        shotgunClient.attach(server, shell);
 
-    // Attach shotgun-client to the http server so it can listen for connections.
-    shotgunClient.attach(server, shell);
-
-    shell.io.set('transports', ['xhr-polling']);
-
-});
+        shell.io.set('transports', ['xhr-polling']);
+    }
+);
